ca758fae6f1591392ebbd56c2c78f21e7c40fef5
CrimsonSoul <ryansbledsoe@gmail.com>
2025-10-11 22:35:58 -0500

    Add PurgeCSS plugin to Vite build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import purgeCss from 'vite-plugin-purgecss'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    purgeCss({
+      content: ['./index.html', './src/**/*.{js,jsx}'],
+      safelist: [
+        'app-shell--radar',
+        'app-main--radar',
+        'app-header__refresh--hidden',
+        'active',
+        'is-selected'
+      ]
+    })
+  ],
   root: '.',
   base: './'
 })
